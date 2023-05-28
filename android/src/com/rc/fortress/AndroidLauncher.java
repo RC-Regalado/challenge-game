975193911abafdce03174c2fd886f03149d26e09
RC-Regalado <ruben.alekey@gmail.com>
2023-05-28 04:24:29 -0600

    Un nuevo comienzo

--- android/src/com/rc/fortress/AndroidLauncher.java
+++ android/src/com/rc/fortress/AndroidLauncher.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.rc.fortress.Fortress;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
--- desktop/src/com/rc/fortress/DesktopLauncher.java
+++ desktop/src/com/rc/fortress/DesktopLauncher.java
@@ -2,15 +2,14 @@ package com.rc.fortress;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.rc.fortress.Fortress;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-
-//		TexturePacker.process("sabre", "test", "sabre2");
+		config.setTitle("Muffin");
 		new Lwjgl3Application(new Fortress(), config);
 	}
 }
